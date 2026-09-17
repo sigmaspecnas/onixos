9d5cba53c601203b98da9387ade350bf9efd78d7
sigmaspecnas <chockgranny@gmail.com>
2026-09-17 09:55:26 +0300

    Fix kde.nix: kdePackages.plasma-browser-integration

--- modules/onix/profiles/kde.nix
+++ modules/onix/profiles/kde.nix
@@ -11,7 +11,7 @@ lib.mkIf (config.onix.profile == "kde") {
 
   environment.systemPackages = with pkgs; [
     kdePackages.kio-admin
-    plasma-browser-integration
+    kdePackages.plasma-browser-integration
     papirus-icon-theme
   ];
 }
